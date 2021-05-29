700b30f214271b0c8ba19a071400c59683cd36de
TheDarkSid3r <sid3rdark@gmail.com>
2021-05-29 10:23:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 $(() => {
     const generateSpaces = (elem) => " ".repeat(Math.max(0, $("#".concat(elem, "-spaces")).val()));
-    const update = () => {
-        $(".output").val($(".input").val().trim().split(" ").map(w => w.split("").join(generateSpaces("char"))).join(generateSpaces("word")));
-    };
-
+    const update = () => $(".output").val($(".input").val().trim().split(" ").map(w => w.split("").join(generateSpaces("char"))).join(generateSpaces("word")));
     $("input,.input").on("input", update);
 });
